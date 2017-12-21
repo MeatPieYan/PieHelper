5925e3f433e1d8090dc3b6cbb1cdb66e4312bb44
YanZhenggang <yanzhenggang@zhongan.com>
2017-12-21 14:31:18 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,13 @@
+const PieComponent = require('./dist/PieComponent');
+const pieAction = require('./dist/pieAction');
+const pieConnect = require('./dist/pieConnect');
+const pieReducer = require('./dist/pieReducer');
+const pieSaga = require('./dist/pieSaga');
+
+module.exports = {
+  PieComponent,
+  pieAction,
+  pieConnect,
+  pieReducer,
+  pieSaga
+};
