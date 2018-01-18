f1979b770511476d69c67a7b2b64ad8987c92fd8
YanZhenggang <yanzhenggang@zhongan.com>
2018-01-18 16:11:21 +0800

    send enter_page action in componentDidMount

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const PieComponent = require('./dist/PieComponent');
 const pieConnect = require('./dist/pieConnect');
-
+const pieAction = require('./dist/pie/pieAction');
 const pieReducer = require('./dist/pie/pieReducer');
 
 module.exports = {
   PieComponent,
   pieConnect,
-  pieReducer
+  pieReducer,
+  pieAction
 };
