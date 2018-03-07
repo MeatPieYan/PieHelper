21b4d7da73a5fe1d83b6eec737c5831a41fb6f43
xuqiuyu <xuqiuyu@zhongan.com>
2018-03-07 11:57:45 +0800

    add phoenixConnect to module exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const PieComponent = require('./dist/PieComponent');
 const pieConnect = require('./dist/pieConnect');
+const phoenixConnect = require('./dist/phoenixConnect');
 const pieAction = require('./dist/pie/pieAction');
 const pieReducer = require('./dist/pie/pieReducer');
 
